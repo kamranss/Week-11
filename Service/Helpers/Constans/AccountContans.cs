cda24d084049a998cbf11c50f096283c7a546810
kamranss <kamranss@code.edu.az>
2022-12-03 23:42:35 +0400

    Feature: Added new Project

---
+++ Service/Helpers/Constans/AccountContans.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service.Helpers.Constans
+{
+    internal class AccountContans
+    {
+        public const string SuperAdminEntrance = "Super admin olaraq giris etdiniz";
+        public const string CredentialsInvalid = "Email or Password are wrong";
+    }
+}
